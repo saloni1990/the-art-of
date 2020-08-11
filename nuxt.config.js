8f4d4a2f4a0149f5346a1cc41d54f629cfce0e6a
Saloni Robinson <you@example.com>
2020-08-11 15:54:58 +0100

    update nuxt confug

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: 'The Art of Product Sourcing' || '',
+    title: 'The Art Of' || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
