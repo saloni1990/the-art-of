e37939cef227d73cb33339711c2c2588d072a9f6
Saloni Robinson <you@example.com>
2020-08-11 19:30:40 +0100

    update package.json and add routes for about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,69 +1,60 @@
 <template>
-  <div class="">
-    <div class="">
-      <Header />
+    <div class="min-h-screen text-center mx-auto bg-gray-200">
+        <div class="bg-white">
+            <Header />
+        <div class="max-w-4xl px-4 mx-auto content">
+            <h1 class="mb-8 italic">{{page.title}}</h1>
+            <div class="w-full flex justify-center pb-4">
+                <cld-image :publicId="`/interactive_agents/${page.pageImage}`"  width="500" crop="fill" />
+            </div>
+            <div v-html="parseMarkdown(page.body)" class="px-4 "></div>
 
-      <div class="w-full max-auto pt-2">
-        <div class="flex flex-wrap justify-center items-center ">
-          <h1>About</h1>
         </div>
-      </div>
-      <Footer />
+        <Footer />
+        </div>
+        
     </div>
-  </div>
 </template>
 
+
 <script>
+
+import marked from 'marked'
 import Header from '~/components/Header.vue'
 import Footer from '~/components/Footer.vue'
+// import cloudinary from 'cloudinary-core';
+// const cloudinaryCore = new cloudinary.Cloudinary({cloud_name: 'smartav'});
+
 
 export default {
+  data() {
+    return {
+      index: 0
+    }
+  },
+
   components: {
     Header,
     Footer
   },
-  head() {
-  return {
-    script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
-  };
+  async asyncData({ params, payload }) {
+    if (payload) return { page: payload }
+    else
+      return {
+        page: await require(`~/assets/content/pages/about/about.json`)
+      }
+  },
+  methods: {
+    parseMarkdown(content) {
+      if (content) {
+        return marked(content)
+      }
+    },
+    // cloudinarySrc(publicId) {
+    //   return cloudinaryCore.url(`/interactive_agents/${publicId}`, { width: 800, height:800, crop: "fit"});
+    // }
   },
 }
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
